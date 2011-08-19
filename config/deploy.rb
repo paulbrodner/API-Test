6f42f56b05bee09fb6ced1169c6617dd79dccc00
Paul Brodner <Paul.Brodner@osf-global.com>
2011-08-19 17:23:41 +0300

    test capistrano 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,55 @@
-require 'fileutils'
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+require 'bundler/capistrano'
+set :rvm_ruby_string, '1.9.2'
+set :rvm_type, :user
+
+before "deploy",         "deploy:check_id_rsa"
+before "deploy",         "deploy:check_pem"
+
 
 # Domains
 set :dev_domain,            'www.dev.doxsite.com'
 
 set :application,           "api-consumer"
+set :scm,                   :git # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
 set :repository,            "git@github.com:paulbrodner/API-Test.git"
+set :branch,                'master'
 
 set :user,                  "ubuntu"
 set :use_sudo,              false
+
 set :deploy_to,             "/var/www/#{application}"
-set :api_config,            "/var/api_consumer_config/current"
 set :deploy_via,            :remote_cache
+set :keep_releases,         2
+
 set :ssh_options,           { :forward_agent => true, :port => 22, :keys => [ "#{ENV['HOME']}/.ssh/osf.pem" ] }
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
 
 role :web, dev_domain                          # Your HTTP server, Apache/etc
 role :app, dev_domain                          # This may be the same as your `Web` server
-role :db,  dev_domain, :primary => true # This is where Rails migrations will run
+role :db,  dev_domain, :primary => true        # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+#after "deploy", "deploy:bundle_gems"
+#after "deploy:bundle_gems", "deploy:restart"
 
-before "deploy",         "deploy:check_id_rsa"
-before "deploy",         "deploy:check_pem"
-#after  "deploy:update_code",  "deploy:copy_files"
 
-# If you are using Passenger mod_rails uncomment this:
+# for Passenger mod_rails 
 namespace :deploy do
+  #  desc "Run Bundle Install"
+  #  #you will see the current gems installed
+  #  task :bundle_gems do
+  #    run "cd #{deploy_to}/current && bundle install vendor/gems"
+  #  end
+  
   task :start do ; end
   task :stop do ; end
+
+  desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 
   task :check_pem do
@@ -40,22 +57,20 @@ namespace :deploy do
     if( !exists )
       puts "it seems there's no pem file (looking for #{ssh_options[:keys][0]})"
       exit
+    else
+      puts "pem file found!!!!"
     end
   end
-
+  
   task :check_id_rsa do
     if( File.exists?( '/home/vagrant/' ) )
       exists = File.exists?( '/home/vagrant/.ssh/id_rsa' )
       if( !exists )
         puts "it seems this is a vagrant VM and there's no id_rsa file (looking for /home/vagrant/.ssh/id_rsa)"
         exit
+      else
+        puts "found id_rsa file!!!"
       end
     end
   end
-
-  task :copy_files do
-    FileUtils.cp_r "#{deploy_to}/shared/cached-copy", "#{deploy_to}/current", :verbose => true
-    FileUtils.cp_r "#{api_config}", "#{deploy_to}/current", :verbose => true
-  end
-  
 end
