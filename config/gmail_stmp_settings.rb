ae2c1a5eebfcbda448cd785d617bdae2ed2fbd36
Paul Brodner <Paul.Brodner@osf-global.com>
2011-09-01 11:08:25 +0300

    remove gmail settings

--- config/gmail_stmp_settings.rb
+++
@@ -1,9 +0,0 @@
-# TODO refactor this into yaml
-STMP_SETTINGS = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :user_name => "",
-    :password => "",
-    :authentication => :plain
-  }
